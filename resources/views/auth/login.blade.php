2af07badfa797e1b0cebac25daf9f608eca72ac5
Wilfred Silayo <wilfredsilayo99@gmail.com>
2023-05-29 20:02:46 +0300

    Final changes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -85,5 +85,18 @@
         </div>
     </div>
 </div>
-<script src="{{asset('scripts/visibility.js')}}"></script>
+<script>
+
+var passwordField = document.getElementById('password');
+
+
+var togglePasswordButton = document.getElementById('togglePassword');
+
+togglePasswordButton.addEventListener('click', function() {
+    if (passwordField.type === 'password') {
+        passwordField.type = 'text';
+    } else {
+        passwordField.type = 'password';
+    }
+});</script>
 @endsection
